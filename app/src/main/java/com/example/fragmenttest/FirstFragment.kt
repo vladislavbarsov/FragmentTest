4dab5c03860f226a18f982a0fbdd1ce2e5cd7a8b
vladislavbarsov <35625801+vladislavbarsov@users.noreply.github.com>
2021-07-31 18:43:41 +0300

    Add synthetics and simplify the Fragment

--- app/src/main/java/com/example/fragmenttest/FirstFragment.kt
+++ app/src/main/java/com/example/fragmenttest/FirstFragment.kt
@@ -1,19 +1,9 @@
 package com.example.fragmenttest
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
-class FirstFragment : Fragment() {
+class FirstFragment : Fragment(R.layout.fragment_first) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
-    }
+
 
 }
