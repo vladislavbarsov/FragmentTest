a4cf6313205417ad493907feef1a196b5e2c2c7a
vladislavbarsov <35625801+vladislavbarsov@users.noreply.github.com>
2021-07-31 19:00:53 +0300

    Add second fragment and navigation between them

--- app/src/main/java/com/example/fragmenttest/MainActivity.kt
+++ app/src/main/java/com/example/fragmenttest/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.fragmenttest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        // Initial setup for first fragment to be displayed
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, firstFragment) // replace FrameLayout with Fragment tag
+            commit() // apply changes
+        }
+
+        btnFragment1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, firstFragment) // replace FrameLayout with Fragment tag
+                addToBackStack(null) // add the fragment to backstack so that the Back button in the navigation panel switches them as well
+                commit() // apply changes
+            }
+        }
+
+        btnFragment2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, secondFragment) // replace FrameLayout with Fragment tag
+                addToBackStack(null) // you can pass the name, but if you dont need - null.
+                commit() // apply changes
+            }
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/fragmenttest/SecondFragment.kt
@@ -0,0 +1,7 @@
+package com.example.fragmenttest
+
+import androidx.fragment.app.Fragment
+
+class SecondFragment : Fragment(R.layout.fragment_second) {
+
+}
